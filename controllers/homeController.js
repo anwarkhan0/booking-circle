20dd972866448c7961c518a3d1b80d92b799ff4c
Anwar khan <37025748+anwarkhan0@users.noreply.github.com>
2022-02-28 14:11:02 +0500

    func added to home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -21,17 +21,57 @@ const home = async (req, res, next) => {
   const sliderGall = await sliderGallery.findOne();
 
   const hotels = await HotelsModel.find();
+  const appartments = await AppartmentModel.find();
+  const tours = await ToursModel.find();
+  const vehicles = await VehiclesModel.find();
+
   const ourHotelsIn = [];
+  const ourtAppartmentsIn = [];
+  const ourToursIn = [];
+  const ourVehiclesIn = [];
 
-  const count = hotels.length;
-  let random = Math.floor(Math.random() * count);
+  function getRandom(count) {
+    let random = Math.floor(Math.random() * count);
 
-  if(random == count){
-    random = 0;
+    if (random == count) {
+      random = 0;
+    }
+
+    return random;
   }
 
-  for(let i= random; i< count; i++){
+  const hotelsRandom = getRandom(hotels.length);
+  const appartmentsRandom = getRandom(appartments.length);
+  const toursRandom = getRandom(tours.length);
+  const vehiclesRandom = getRandom(vehicles.length);
+  
+
+  for(let i= hotelsRandom; i< hotels.length; i++){
+    if(typeof hotels[i] === undefined){
+      break;
+    }
     ourHotelsIn.push(hotels[i]);
+  }
+
+  for(let i= appartmentsRandom; i< appartments.length; i++){
+    if(typeof appartments[i] === undefined){
+      break;
+    }
+    ourtAppartmentsIn.push(appartments[i]);
+  }
+
+  for(let i= toursRandom; i< tours.length; i++){
+    if(typeof tours[i] === undefined){
+      break;
+    }
+    ourToursIn.push(tours[i]);
+  }
+
+  for(let i= vehiclesRandom; i< vehicles.length; i++){
+    if(typeof vehicles[i] === undefined){
+      break;
+    }
+    ourVehiclesIn.push(vehicles[i]);
   }
 
   res.render("./pages/HomePage/home", {
