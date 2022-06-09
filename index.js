104273bfe67dbd8dc1775eb8a6da3871e6ef5946
anwarkhan0 <anwarsyed01234@gmail.com>
2022-06-09 15:07:28 +0500

    config changed for heroku

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(compression());
 
-cloud
+// cloud
 const store = new MongoDBStore({
   uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjk47.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   collection: 'sessions'
